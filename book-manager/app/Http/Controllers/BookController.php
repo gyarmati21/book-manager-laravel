6471db218f1c370f5a228bf82dcabc9b3de1345d
gyarmati21 <gyarmatipatrik2001@gmail.com>
2024-09-17 11:40:05 +0200

    feat: filtering/order by title, genre, release date

--- book-manager/app/Http/Controllers/BookController.php
+++ book-manager/app/Http/Controllers/BookController.php
@@ -7,12 +7,44 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $books = Book::get();
-
-        return view('book.index', compact('books'));
+        //parameters for filtering and sorting
+        $search = $request->input('search');
+        $genre = $request->input('genre');
+        $release_date_from = $request->input('release_date_from');
+        $release_date_to = $request->input('release_date_to');
+        $order = $request->input('order', 'asc');  // Default to ascending order
+        $sort_by = $request->input('sort_by', 'title'); // Default sorting by title
+ 
+        $query = Book::query();
+    
+        //search (title)
+        if ($search) {
+            $query->where('title', 'like', '%' . $search . '%');
+        }
+    
+        //genre
+        if ($genre) {
+            $query->where('genre', $genre);
+        }
+    
+        //release date between two dates
+        if ($release_date_from && $release_date_to) {
+            $query->whereBetween('release_date', [$release_date_from, $release_date_to]);
+        }
+    
+        //sort by field and order
+        $query->orderBy($sort_by, $order);
+    
+        //get  books
+        $books = $query->get();
+    
+     
+        return view('book.index', compact('books', 'search', 'genre', 'release_date_from', 'release_date_to', 'order', 'sort_by'));
     }
+    
+    
 
     public function create()
     {
--- book-manager/resources/views/book/index.blade.php
+++ book-manager/resources/views/book/index.blade.php
@@ -1,17 +1,68 @@
 <x-app-layout>
-
     <x-slot name="title">
         Books
     </x-slot>
+
     <div class="container mt-5">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <h4>Books
-                        <a href="{{ url('books/create')}}" class="btn btn-primary float-end">Add Book</a>
+                        <a href="{{ url('books/create') }}" class="btn btn-primary float-end">Add Book</a>
                     </h4>
                 </div>
                 <div class="card-body">
+                    <!-- Search and Filtering Form -->
+                    <form action="{{ url('books') }}" method="GET" class="mb-4">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="">Search by Title:</label>
+                                <input type="text" name="search" value="{{ request('search') }}" class="form-control" placeholder="Search by Title">
+                            </div>
+                            <div class="col-md-2">
+                                <label for="">Genre:</label>
+                                <select name="genre" class="form-control">
+                                    <option value="">All Genres</option>
+                                    @foreach (App\Models\Book::GENRES as $genreOption)
+                                        <option value="{{ $genreOption }}" {{ request('genre') == $genreOption ? 'selected' : '' }}>
+                                            {{ $genreOption }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-2">
+                                <label for="">Released From:</label>
+                                <input type="date" name="release_date_from" value="{{ request('release_date_from') }}" class="form-control" placeholder="From Date">
+                            </div>
+                            <div class="col-md-2">
+                                <label for="">Released To:</label>
+                                <input type="date" name="release_date_to" value="{{ request('release_date_to') }}" class="form-control" placeholder="To Date">
+                            </div>
+                            <div class="col-md-1">
+                                <label for="">Sort by:</label>
+                                <select name="sort_by" class="form-control">
+                                    <option value="title" {{ request('sort_by') == 'title' ? 'selected' : '' }}>Sort by Title</option>
+                                    <option value="release_date" {{ request('sort_by') == 'release_date' ? 'selected' : '' }}>Sort by Release Date</option>
+                                    <option value="author" {{ request('sort_by') == 'author' ? 'selected' : '' }}>Sort by Author</option>
+                                </select>
+                            </div>
+                            <div class="col-md-1">
+                                <label for="">Order:</label>
+                                <select name="order" class="form-control">
+                                    <option value="asc" {{ request('order') == 'asc' ? 'selected' : '' }}>Asc</option>
+                                    <option value="desc" {{ request('order') == 'desc' ? 'selected' : '' }}>Desc</option>
+                                </select>
+                            </div>
+                            
+                            <div class="col-md-4 d-flex mt-3">
+                                <button type="submit" class="btn btn-primary w-100 mx-1">Filter</button>
+                                <a href="{{ url('books') }}" class="btn btn-secondary w-50 mx-1">Reset</a>
+                            </div>
+                            
+                        </div>
+                    </form>
+
+                    <!-- Books Table -->
                     <table class="table table-bordered table-striped">
                         <thead>
                             <tr>
@@ -29,21 +80,20 @@
                         <tbody>
                             @foreach ($books as $item)
                                 <tr>
-                                    <td>{{$item->id}}</td>
-                                    <td>{{$item->title}}</td>
-                                    <td>{{$item->author}}</td>
-                                    <td>{{$item->genre}}</td>
-                                    <td>{{$item->release_date}}</td>
-                                    <td>{{$item->description}}</td>
-                                    <td>{{$item->cover_image}}</td>
-                                    <td>{{$item->created_at}}</td>
-                                    <td>{{$item->updated_at}}</td>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->title }}</td>
+                                    <td>{{ $item->author }}</td>
+                                    <td>{{ $item->genre }}</td>
+                                    <td>{{ $item->release_date }}</td>
+                                    <td>{{ $item->description }}</td>
+                                    <td>{{ $item->cover_image }}</td>
+                                    <td>{{ $item->created_at }}</td>
+                                    <td>{{ $item->updated_at }}</td>
                                     <td>
                                         <a href="{{ url('books/'.$item->id.'/edit') }}" class="btn btn-success mx-2">Edit</a>
-                                        <a href="{{ url('books/'.$item->id.'/delete') }}" 
-                                            class="btn btn-danger mx-1"
-                                            onclick="return confirm('Are you sure you want to delete this book?')"
-                                            >Delete</a>
+                                        <a href="{{ url('books/'.$item->id.'/delete') }}" class="btn btn-danger mx-1"
+                                           onclick="return confirm('Are you sure you want to delete this book?')">Delete</a>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
